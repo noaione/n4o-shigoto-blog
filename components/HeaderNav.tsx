e930e40e4a88a6ecc693c62920008cf7c01f9052
noaione <noaione0809@gmail.com>
2022-05-31 17:36:26 +0800

    Rework the navigation for posts

---
+++ components/HeaderNav.tsx
@@ -0,0 +1,48 @@
+import Link from "next/link";
+import React from "react";
+import MobileNav from "./MobileNav";
+
+interface HeaderProps {
+    isTags?: boolean;
+}
+
+export default function HeaderNav(props: HeaderProps) {
+    const { isTags } = props;
+
+    return (
+        <header className="flex items-center justify-between py-10">
+            <div>
+                <a className="break-words" aria-label="Blog Home" href="/release">
+                    <div className="flex items-center justify-between hover:underline">
+                        <div className="hidden h-6 text-2xl font-bold sm:block">N4O Release</div>
+                    </div>
+                </a>
+            </div>
+            <div className="flex items-center text-base leading-5">
+                <div className="hidden sm:block">
+                    <Link href="/" passHref>
+                        <a className="break-words p-1 font-medium text-gray-900 sm:p-4 dark:text-gray-100 hover:opacity-80 transition-opacity duration-150">
+                            Home
+                        </a>
+                    </Link>
+                    <Link href={isTags ? "/release" : "/release/tags"} passHref>
+                        <a className="break-words p-1 font-medium text-gray-900 sm:p-4 dark:text-gray-100 hover:opacity-80 transition-opacity duration-150">
+                            {isTags ? "Posts" : "Tags"}
+                        </a>
+                    </Link>
+                    <Link href="/feed.xml" passHref>
+                        <a className="break-words p-1 font-medium text-gray-900 sm:p-4 dark:text-gray-100 hover:opacity-80 transition-opacity duration-150">
+                            RSS
+                        </a>
+                    </Link>
+                    <Link href="https://github.com/noaione" passHref>
+                        <a className="break-words p-1 font-medium text-gray-900 sm:p-4 dark:text-gray-100 hover:opacity-80 transition-opacity duration-150">
+                            GitHub
+                        </a>
+                    </Link>
+                </div>
+                <MobileNav isTags={isTags} />
+            </div>
+        </header>
+    );
+}
--- pages/release/index.tsx
+++ pages/release/index.tsx
@@ -1,3 +1,4 @@
+import HeaderNav from "@/components/HeaderNav";
 import MagnifyingGlassOutline from "@/components/Icons/MagnifyingGlassOutline";
 import MetadataHead from "@/components/MetadataHead";
 import NaoTimesEmbed from "@/components/NaoTimesEmbed";
@@ -44,14 +45,17 @@ function PostViewer(props: FrontMatterExtended) {
         firstImage = props.images[0];
     }
 
+    // @ts-ignore
+    const summary = props.excerpt || props.summary;
+
     return (
         <div>
+            {firstImage && (
+                <div className="prose">
+                    <img className="max-w-full mb-2" src={firstImage} alt={props.title + " Thumbnail"} />
+                </div>
+            )}
             <h3 className="text-2xl font-bold leading-8 tracking-tight">
-                {firstImage && (
-                    <div className="prose">
-                        <img className="max-w-full mb-2" src={firstImage} alt={props.title + " Thumbnail"} />
-                    </div>
-                )}
                 <Link href={`/r/${props.slug}`} passHref>
                     <a className="break-words text-gray-900 dark:text-gray-100 hover:underline">
                         {props.layout.toLowerCase() === "bdmv" ? <>{`[BDMV] ${props.title}`}</> : props.title}
@@ -59,16 +63,14 @@ function PostViewer(props: FrontMatterExtended) {
                 </Link>
             </h3>
             <div className="prose text-lg mt-1 text-gray-500 max-w-none dark:text-gray-400">
-                <p>{props.summary}</p>
+                <p>{summary}</p>
             </div>
             <div className="mt-2">
                 {props.tags && props.tags.length > 0 && (
                     <div className="flex flex-row gap-1">
                         {props.tags.map((tag: string) => (
-                            <Link href={`/release/tags/${tag}`} passHref>
-                                <a key={`tagar-${tag}`} className="text-blue-500 hover:opacity-70 transition">
-                                    {"#" + tag}
-                                </a>
+                            <Link key={`tagar-${props.slug}-${tag}`} href={`/release/tags/${tag}`} passHref>
+                                <a className="text-blue-500 hover:opacity-70 transition">{"#" + tag}</a>
                             </Link>
                         ))}
                     </div>
@@ -88,52 +90,16 @@ export default function ShigotoIndexPage({ posts, pagination }: ShigotoIndexProp
             </Head>
             <div className="max-w-3xl px-4 mx-auto sm:px-6 xl:max-w-5xl xl:px-0">
                 <div className="flex flex-col justify-between h-screen">
-                    <header className="flex items-center justify-between py-10">
-                        <div>
-                            <a className="break-words" aria-label="Blog Home" href="/release">
-                                <div className="flex items-center justify-between hover:underline">
-                                    <div className="hidden h-6 text-2xl font-bold sm:block">N4O Release</div>
-                                </div>
-                            </a>
-                        </div>
-                        <div className="flex items-center text-base leading-5">
-                            <div className="hidden sm:block">
-                                <a
-                                    className="break-words p-1 font-medium text-gray-900 sm:p-4 dark:text-gray-100 hover:opacity-80 transition-opacity duration-150"
-                                    href="/"
-                                >
-                                    Home
-                                </a>
-                                <a
-                                    className="break-words p-1 font-medium text-gray-900 sm:p-4 dark:text-gray-100 hover:opacity-80 transition-opacity duration-150"
-                                    href="/release/tags"
-                                >
-                                    Tags
-                                </a>
-                                <a
-                                    className="break-words p-1 font-medium text-gray-900 sm:p-4 dark:text-gray-100 hover:opacity-80 transition-opacity duration-150"
-                                    href="/feed.xml"
-                                >
-                                    RSS
-                                </a>
-                                <a
-                                    className="break-words p-1 font-medium text-gray-900 sm:p-4 dark:text-gray-100 hover:opacity-80 transition-opacity duration-150"
-                                    href="https://github.com/noaione"
-                                >
-                                    GitHub
-                                </a>
-                            </div>
-                        </div>
-                    </header>
+                    <HeaderNav />
                     <main className="mb-auto">
                         <div className="max-w-3xl px-4 mx-auto sm:px-6 xl:max-w-5xl xl:px-0">
                             <article>
                                 <div className="xl:divide-y xl:divide-gray-200 xl:dark:divide-gray-700">
                                     <div className="pt-6 pb-8 space-y-2 md:space-y-5">
                                         <h1 className="text-4xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:leading-10 md:leading-14 ">
-                                            Release
+                                            Releases
                                         </h1>
-                                        <div className="relative max-w-lg">
+                                        <div className="relative max-w-lg pt-2">
                                             <input
                                                 type="text"
                                                 className="block w-full px-4 py-2 text-gray-900 bg-white border border-gray-300 rounded-md dark:border-gray-900 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-900 dark:text-gray-100 transition"
@@ -144,6 +110,7 @@ export default function ShigotoIndexPage({ posts, pagination }: ShigotoIndexProp
                                             <MagnifyingGlassOutline className="absolute w-5 h-5 text-gray-400 right-3 top-3 dark:text-gray-300" />
                                         </div>
                                     </div>
+                                    <hr className="block xl:hidden border-gray-200 dark:border-gray-700" />
                                     <div
                                         className="pb-8 divide-y divide-gray-200 xl:divide-y-0 dark:divide-gray-700 xl:grid xl:grid-cols-3 xl:gap-x-6"
                                         style={{
@@ -159,7 +126,7 @@ export default function ShigotoIndexPage({ posts, pagination }: ShigotoIndexProp
                                             <div className="pt-10 pb-8 prose dark:prose-dark max-w-none">
                                                 <ul className="divide-y divide-gray-200 dark:divide-gray-700">
                                                     {posts.map((post) => (
-                                                        <li key={post.slug} className="py-2">
+                                                        <li key={post.slug} className="py-4">
                                                             <PostViewer key={post.slug} {...post} />
                                                         </li>
                                                     ))}
@@ -179,7 +146,10 @@ export default function ShigotoIndexPage({ posts, pagination }: ShigotoIndexProp
                                                         Project Status
                                                     </h2>
                                                     <div className="mt-2">
-                                                        <NaoTimesEmbed serverId="472705451117641729" />
+                                                        <NaoTimesEmbed
+                                                            serverId="472705451117641729"
+                                                            accentColor="blue"
+                                                        />
                                                     </div>
                                                 </div>
                                             </div>
