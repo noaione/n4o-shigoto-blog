34bb88f199914ba3b8dcc4912731119028b6d205
noaione <noaione0809@gmail.com>
2022-05-31 17:11:27 +0800

    Add icon to document

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -51,6 +51,27 @@ export default class ShigotoDocument extends Document {
                     <meta charSet="utf-8" />
                     <meta httpEquiv="X-UA-Compatible" content="IE=edge,chrome=1" />
                     <InlineJs code={THEMECHECKERJS} />
+                    {/* Favicons */}
+                    <link rel="shortcut icon" type="image/png" href="/assets/ico/favicon.png" />
+                    <link rel="shortcut icon" href="/favicon.ico" />
+                    <link rel="apple-touch-icon" sizes="57x57" href="/assets/ico/apple-icon-57x57.png" />
+                    <link rel="apple-touch-icon" sizes="60x60" href="/assets/ico/apple-icon-60x60.png" />
+                    <link rel="apple-touch-icon" sizes="72x72" href="/assets/ico/apple-icon-72x72.png" />
+                    <link rel="apple-touch-icon" sizes="76x76" href="/assets/ico/apple-icon-76x76.png" />
+                    <link rel="apple-touch-icon" sizes="114x114" href="/assets/ico/apple-icon-114x114.png" />
+                    <link rel="apple-touch-icon" sizes="120x120" href="/assets/ico/apple-icon-120x120.png" />
+                    <link rel="apple-touch-icon" sizes="144x144" href="/assets/ico/apple-icon-144x144.png" />
+                    <link rel="apple-touch-icon" sizes="152x152" href="/assets/ico/apple-icon-152x152.png" />
+                    <link rel="apple-touch-icon" sizes="180x180" href="/assets/ico/apple-icon-180x180.png" />
+                    <link
+                        rel="icon"
+                        type="image/png"
+                        sizes="192x192"
+                        href="/assets/ico/android-icon-192x192.png"
+                    />
+                    <link rel="icon" type="image/png" sizes="32x32" href="/assets/ico/favicon-32x32.png" />
+                    <link rel="icon" type="image/png" sizes="96x96" href="/assets/ico/favicon-96x96.png" />
+                    <link rel="icon" type="image/png" sizes="16x16" href="/assets/ico/favicon-16x16.png" />
                 </Head>
                 <body className="bg-white text-black dark:bg-gray-800 dark:text-gray-100">
                     <Main />
