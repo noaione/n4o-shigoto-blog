78b358957d7feeef01e2d711b2ced35e8c5dd9fe
noaione <noaione0809@gmail.com>
2022-08-02 13:04:29 +0800

    pages(manga): add backlog data renderer

--- pages/manga/index.tsx
+++ pages/manga/index.tsx
@@ -5,7 +5,7 @@ import type { FrontMatterExtended } from "@/lib/mdx";
 import Link from "next/link";
 import Head from "next/head";
 import MetadataHead from "@/components/MetadataHead";
-import { isNone } from "@/lib/utils";
+import { isNone, NoneType, Nullable } from "@/lib/utils";
 
 export async function getStaticProps() {
     const { getAllPostsFrontMatter } = await import("@/lib/mdx");
@@ -37,6 +37,69 @@ interface HotlinksReleasing {
 type InnerUpcomingReleases = { [title: string]: HotlinksReleasing[] };
 type UpcomingReleases = { [dateData: string]: InnerUpcomingReleases };
 
+function parseMangaReleaseData(
+    hotlink: SimpleHotlinks | string,
+    manga: FrontMatterManga,
+    targetYear: Nullable<number> = null,
+    targetMonth: Nullable<number> = null,
+    targetDay: Nullable<number> = null,
+    ignoreDay: boolean = false
+): [HotlinksReleasing, string] | NoneType {
+    let selectTitle: string | undefined;
+    if (typeof hotlink === "string") {
+        selectTitle = hotlink;
+    } else {
+        if (!isNone(hotlink.title)) {
+            selectTitle = hotlink.title;
+        }
+    }
+
+    if (isNone(selectTitle)) {
+        return;
+    }
+
+    const match = VolumeWaitRe.exec(selectTitle);
+    if (isNone(match)) {
+        return;
+    }
+    if (isNone(match.groups)) {
+        return;
+    }
+
+    const { date } = match.groups;
+    if (isNone(date)) {
+        return;
+    }
+
+    const parsedDate = DateTime.fromFormat(date, "d MMMM yyyy", { zone: "UTC" });
+    if (!parsedDate.isValid) {
+        return;
+    }
+    if (!isNone(targetYear) && parsedDate.year !== targetYear) {
+        return;
+    }
+    // check if it's in current month
+    if (!isNone(targetMonth) && parsedDate.month !== targetMonth) {
+        return;
+    }
+    // check if current date is larger than parsed date
+    if (!ignoreDay && !isNone(targetDay) && targetDay > parsedDate.day) {
+        return;
+    }
+    const dateFmt = parsedDate.toFormat("yyyy-MM-dd");
+    const mangaTitle = manga.title.trim();
+    return [
+        {
+            date: parsedDate,
+            title: mangaTitle,
+            slug: manga.slug,
+            volume: Number(match.groups.vol),
+            extra: match.groups.ex?.trimEnd(),
+        },
+        dateFmt,
+    ];
+}
+
 function getUpcomingReleaseAtMonth(
     mangas: FrontMatterManga[],
     year: number,
@@ -55,61 +118,86 @@ function getUpcomingReleaseAtMonth(
         }
 
         hotlinks.forEach((hotlink) => {
-            let selectTitle: string | undefined;
-            if (typeof hotlink === "string") {
-                selectTitle = hotlink;
-            } else {
-                if (!isNone(hotlink.title)) {
-                    selectTitle = hotlink.title;
-                }
-            }
-
-            if (isNone(selectTitle)) {
+            const result = parseMangaReleaseData(hotlink, manga, year, month, currentTime.day, ignoreDay);
+            if (isNone(result)) {
                 return;
             }
 
-            const match = VolumeWaitRe.exec(selectTitle);
-            if (isNone(match)) {
-                return;
-            }
-            if (isNone(match.groups)) {
-                return;
-            }
+            const [data, dateFmt] = result;
 
-            const { date } = match.groups;
-            if (isNone(date)) {
-                return;
-            }
-
-            const parsedDate = DateTime.fromFormat(date, "d MMMM yyyy", { zone: "UTC" });
-            if (!parsedDate.isValid) {
-                return;
-            }
-            if (parsedDate.year !== year) {
-                return;
-            }
-            // check if it's in current month
-            if (parsedDate.month !== month) {
-                return;
-            }
-            // check if current date is larger than parsed date
-            if (!ignoreDay && currentTime.day > parsedDate.day) {
-                return;
-            }
-            const dateFmt = parsedDate.toFormat("yyyy-MM-dd");
-            const mangaTitle = manga.title.trim();
             filteredReleases[dateFmt] = filteredReleases[dateFmt] || {};
-            filteredReleases[dateFmt][mangaTitle] = filteredReleases[dateFmt][mangaTitle] || [];
-            filteredReleases[dateFmt][mangaTitle].push({
-                date: parsedDate,
-                title: mangaTitle,
-                slug: manga.slug,
-                volume: Number(match.groups.vol),
-                extra: match.groups.ex?.trimEnd(),
-            });
+            filteredReleases[dateFmt][data.title] = filteredReleases[dateFmt][data.title] || [];
+            filteredReleases[dateFmt][data.title].push(data);
         });
     });
     return filteredReleases;
+}
+
+function sortUpcomingReleaseData(unsortedData: UpcomingReleases) {
+    const temporarySortedData = Object.keys(unsortedData)
+        .sort()
+        .map((month) => {
+            const selected = unsortedData[month];
+            const rlsSortedByTitles = Object.keys(selected)
+                .sort()
+                .map((title) => {
+                    const selectedTitle = selected[title];
+                    // sort by volume
+                    const sortedVolumes = selectedTitle.sort((a, b) => a.volume - b.volume);
+                    const finalSortedVolume: { [key: string]: HotlinksReleasing[] } = {};
+                    finalSortedVolume[title] = sortedVolumes;
+                    return finalSortedVolume;
+                });
+            const finalSortedMonth: UpcomingReleases = {};
+            const temporaryMonthData: InnerUpcomingReleases = {};
+            rlsSortedByTitles.forEach((rls) => {
+                const firstKey = Object.keys(rls)[0];
+                temporaryMonthData[firstKey] = rls[firstKey];
+            });
+            finalSortedMonth[month] = temporaryMonthData;
+            return finalSortedMonth;
+        });
+    const sortedReleases: UpcomingReleases = {};
+    temporarySortedData.forEach((month) => {
+        const firstKey = Object.keys(month)[0];
+        sortedReleases[firstKey] = month[firstKey];
+    });
+    return sortedReleases;
+}
+
+function getBacklogRelease(mangas: FrontMatterManga[]) {
+    const currentTime = DateTime.utc();
+    // get anything that's older by 7 days
+    const backlogStart = currentTime.minus({ days: 7 });
+    const backloggedReelase: UpcomingReleases = {};
+    mangas.forEach((manga) => {
+        const { hotlinks } = manga;
+        if (!Array.isArray(hotlinks)) {
+            return;
+        }
+        if (hotlinks.length < 1) {
+            return;
+        }
+
+        hotlinks.forEach((hotlink) => {
+            const result = parseMangaReleaseData(hotlink, manga, null, null, null, true);
+            if (isNone(result)) {
+                return;
+            }
+
+            const [data, dateFmt] = result;
+            const reparseDate = DateTime.fromFormat(dateFmt, "yyyy-MM-dd");
+            if (reparseDate.toMillis() > backlogStart.toMillis()) {
+                return;
+            }
+
+            backloggedReelase[dateFmt] = backloggedReelase[dateFmt] || {};
+            backloggedReelase[dateFmt][data.title] = backloggedReelase[dateFmt][data.title] || [];
+            backloggedReelase[dateFmt][data.title].push(data);
+        });
+    });
+
+    return sortUpcomingReleaseData(backloggedReelase);
 }
 
 function getStartOfLastWeekOfMonth() {
@@ -145,36 +233,7 @@ function filterMonthHotlinks(mangas: FrontMatterManga[]) {
         filteredReleases = { ...currentMonthData, ...nextMonthData };
     }
 
-    const temporarySortedData = Object.keys(filteredReleases)
-        .sort()
-        .map((month) => {
-            const selected = filteredReleases[month];
-            const rlsSortedByTitles = Object.keys(selected)
-                .sort()
-                .map((title) => {
-                    const selectedTitle = selected[title];
-                    // sort by volume
-                    const sortedVolumes = selectedTitle.sort((a, b) => a.volume - b.volume);
-                    const finalSortedVolume: { [key: string]: HotlinksReleasing[] } = {};
-                    finalSortedVolume[title] = sortedVolumes;
-                    return finalSortedVolume;
-                });
-            const finalSortedMonth: UpcomingReleases = {};
-            const temporaryMonthData: InnerUpcomingReleases = {};
-            rlsSortedByTitles.forEach((rls) => {
-                const firstKey = Object.keys(rls)[0];
-                temporaryMonthData[firstKey] = rls[firstKey];
-            });
-            finalSortedMonth[month] = temporaryMonthData;
-            return finalSortedMonth;
-        });
-    const sortedReleases: UpcomingReleases = {};
-    temporarySortedData.forEach((month) => {
-        const firstKey = Object.keys(month)[0];
-        sortedReleases[firstKey] = month[firstKey];
-    });
-
-    return sortedReleases;
+    return sortUpcomingReleaseData(filteredReleases);
 }
 
 interface StaticPropsData {
@@ -185,6 +244,7 @@ export default function MangaIndexPage({ posts }: StaticPropsData) {
     const rippedMangaRelease = posts.filter((e) => e?.type === "rip");
     const scanMangaRelease = posts.filter((e) => e?.type === "scanlation");
     const thisMonthRelease = filterMonthHotlinks(rippedMangaRelease);
+    const backloggedRelease = getBacklogRelease(rippedMangaRelease);
 
     return (
         <>
@@ -266,6 +326,54 @@ export default function MangaIndexPage({ posts }: StaticPropsData) {
                         </p>
                     )}
                 </div>
+                {Object.keys(backloggedRelease).length > 0 && (
+                    <div className="flex flex-col mt-2 mx-auto px-4">
+                        <h2 className="text-lg font-medium">Backlogged Releases</h2>
+                        {Object.keys(backloggedRelease).map((date) => {
+                            const monthManga = backloggedRelease[date];
+                            const parsedDt = DateTime.fromFormat(date, "yyyy-MM-dd", { zone: "UTC" });
+                            return (
+                                <div key={`backlog-monthly-${date}`} className="flex flex-col">
+                                    <h3 className="font-medium">{parsedDt.toFormat("dd MMMM yyyy")}</h3>
+                                    <div className="flex flex-col flex-wrap justify-center">
+                                        {Object.keys(monthManga).map((title) => (
+                                            <div
+                                                className="flex flex-col"
+                                                key={"backlog-rls-month-" + date + "-" + title}
+                                            >
+                                                <h4 className="text-gray-700 dark:text-gray-300">{title}</h4>
+                                                <ul className="flex flex-col flex-wrap justify-center">
+                                                    {monthManga[title].map((rls) => (
+                                                        <li
+                                                            key={`backlog-rls-month-${date}-${title}-v${rls.volume}`}
+                                                            className="pl-2 text-gray-700 dark:text-gray-300 before:content-['•_']"
+                                                        >
+                                                            <Link href={`/manga/${rls.slug}`} passHref>
+                                                                <a className="transition text-gray-700 dark:text-gray-300 hover:text-red-500 dark:hover:text-red-500">
+                                                                    <span>
+                                                                        Volume {rls.volume.toLocaleString()}
+                                                                    </span>
+                                                                    {rls.extra && (
+                                                                        <span className="italic font-semibold">
+                                                                            {" "}
+                                                                            {rls.extra
+                                                                                .replace("[", "(")
+                                                                                .replace("]", ")")}
+                                                                        </span>
+                                                                    )}
+                                                                </a>
+                                                            </Link>
+                                                        </li>
+                                                    ))}
+                                                </ul>
+                                            </div>
+                                        ))}
+                                    </div>
+                                </div>
+                            );
+                        })}
+                    </div>
+                )}
                 <div className="flex flex-col mt-2 mx-auto px-4">
                     <h2 className="text-lg font-medium">Ripped Release</h2>
                     {rippedMangaRelease.length > 0 ? (
